d57c36f92d41f03e9fa90079eaccbd055c605669
Adam Sutcliffe <adam.sutcliffe@yahoo.co.uk>
2019-03-28 23:17:10 +0000

    [ADD] tweaks to rotation calculation [TODO] deal with odd Quaternion issue -: https://answers.unity.com/questions/596313/rotation-reset-after-90-degre es.html

--- Touch Test/Assets/CubeTouchScript.cs
+++ Touch Test/Assets/CubeTouchScript.cs
@@ -24,12 +24,13 @@ public class CubeTouchScript : MonoBehaviour
     private void OnEnable()
     {
         flickGesture = GetComponent<FlickGesture>();
-        flickGesture.Flicked += flickedHandler;
+        //flickGesture.Flicked += flickedHandler;
         flickGesture.MinDistance = 0.5f;
         flickGesture.MovementThreshold = 0.25f;
         print($"FLICK min: {flickGesture.MinDistance} threshold: {flickGesture.MovementThreshold} time: {flickGesture.FlickTime}");
 
         transformGesture = GetComponent<TransformGesture>();
+        transformGesture.TransformStarted += transformedStartedHandler;
         transformGesture.Transformed += transformedHandler;
         transformGesture.TransformCompleted += transformedEndedHandler;
         //transformGesture.AddFriendlyGesture(flickGesture);
@@ -38,7 +39,9 @@ public class CubeTouchScript : MonoBehaviour
     private void OnDisable()
     {
         flickGesture.Flicked -= flickedHandler;
+        transformGesture.TransformStarted -= transformedStartedHandler;
         transformGesture.Transformed -= transformedHandler;
+        transformGesture.TransformCompleted -= transformedEndedHandler;
     }
 
     private void flickedHandler(object sender, System.EventArgs e)
@@ -51,6 +54,12 @@ public class CubeTouchScript : MonoBehaviour
         //}
     }
 
+    private void transformedStartedHandler(object sender, System.EventArgs e)
+    {
+        print($"transform started - {isFlicked} => {transform.rotation.eulerAngles}");
+    }
+
+
     private void transformedHandler(object sender, System.EventArgs e)
     {
         Vector2 diff = transformGesture.ScreenPosition - transformGesture.PreviousScreenPosition;
@@ -60,7 +69,7 @@ public class CubeTouchScript : MonoBehaviour
         {
             if (!isFlicked)
             {
-                print($"transform changed");
+                //print($"transform changed");
                 float normalisedDiffY = diff.y / screenSize.y;
                 float increment = 90.0f * normalisedDiffY;
                 transform.Rotate(increment, 0, 0);
@@ -70,33 +79,40 @@ public class CubeTouchScript : MonoBehaviour
 
     private void transformedEndedHandler(object sender, System.EventArgs e)
     {
-        print($"transform ended - {isFlicked}");
+        print($"transform ended - {isFlicked} => {transform.rotation.eulerAngles}");
     }
 
     private IEnumerator RotateCube(bool up)
     {
+        print($"Rotate cube start transform: {transform.rotation.eulerAngles}");
         currentSide += (up ? 1 : -1);
         int endRotation = 90 * currentSide;
 
         int counter = 0;
 
         float currentRotation = transform.rotation.eulerAngles.x;
-        float remainder = 90.0f - (currentRotation % 90.0f);
-        float newRotation = currentRotation + 90.0f - remainder;
-        print($"Rotation = current: {currentRotation} end: {endRotation} remainder: {remainder} new: {newRotation}");
+        float eulerYCheck = transform.rotation.eulerAngles.y;
+        float remainder = endRotation - currentRotation;
+        print($"Rotation = current: {currentRotation} end: {endRotation} remainder: {remainder} euler Y: {eulerYCheck}");
 
-        float increment =  remainder / rotationFrames;
+        float increment = remainder / rotationFrames;
         float spaces = rotationTime / rotationFrames;
 
-        print($"Rotate cube increment: {increment} space: {spaces} transform: {transform.rotation.eulerAngles}  remainder: {remainder}");
-
+        print($"Rotate cube increment: {increment} space: {spaces} frames: {rotationFrames}");
+        
         while (counter < rotationFrames)
         {
-            transform.Rotate(increment, 0, 0);
+            //transform.Rotate(increment, 0, 0);
             counter += 1;
+            transform.rotation = Quaternion.Euler(currentRotation + (increment * counter), 0, 0);
             yield return new WaitForSeconds(spaces);
         }
         isFlicked = false;
         print($"Rotate cube end transform: {transform.rotation.eulerAngles}");
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 500, 20), $"{transform.rotation.eulerAngles} - ({transform.rotation})");
     }
 }
